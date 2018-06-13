6244510977422d3634a524d0eedfb0375ef787ca
Akinyele Thompson <akinyelethompson@gmail.com>
2018-06-13 09:47:46 -0500

    misc

---
+++ app/src/main/java/ctrl_it/com/jm/wimmg/fragments/trackerFragment/views/DialogCreateCategory.java
@@ -0,0 +1,18 @@
+package ctrl_it.com.jm.wimmg.fragments.trackerFragment.views;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.widget.FrameLayout;
+
+import butterknife.ButterKnife;
+
+public class DialogCreateCategory extends FrameLayout {
+
+
+    public DialogCreateCategory(@NonNull Context context) {
+        super(context);
+        ButterKnife.bind(this);
+    }
+
+
+}
